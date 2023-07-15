e4d231a021c426ed6e9fbc9851b44d75c54de65b
Leonardo Acevedo <stenoner90@gmail.com>
2023-07-15 05:29:01 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,70 @@
+import discord, responses, re, os
+from discord import app_commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+WHITE_LIST = [224957447096238081]
+BLACK_LIST = [283333073532223488]
+CURRENT_PREFIX = '?'
+
+async def send_message(message: discord.Message, user_mesage: str, is_private: bool):
+    try:
+        response = responses.handle_response(message.channel.id, user_mesage)
+        if len(response) > 0:
+            await message.author.send(response) if is_private else await message.channel.send(response)
+            
+    except Exception as e:
+        print(e)
+
+async def add_reaction(reaction: discord.Reaction, user: discord.User):
+    try:
+        await reaction.message.add_reaction('💖')     
+    except Exception as e:
+        print(e)
+        
+def run_discord_bot():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+    tree = app_commands.CommandTree(client)
+    
+    @client.event
+    async def on_ready():
+        await tree.sync(guild=discord.Object(id=322616525527973890))
+        print(f'{client.user} is now running!')
+        
+    @client.event
+    async def on_message(message: discord.Message):
+        if message.author.id not in WHITE_LIST or message.author == client.user or message.author.bot:
+            return
+        user_name = str(message.author.name)
+        user_message = str(message.content)
+        channel = str(message.channel.name)
+        
+        print(f'{user_name} said: {user_message} in {channel}')
+        
+        if len(user_message) > 0 and re.search(f'^\{CURRENT_PREFIX}.+', user_message):
+            command = user_message[1:]
+            await send_message(message, command, is_private=False)
+            
+    @client.event
+    async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
+        if user.id not in WHITE_LIST or user.bot:
+            return
+        await add_reaction(reaction, user)
+    
+    @tree.command(name='roll', description= 'Mudae rolls', guild=discord.Object(id=322616525527973890))
+    @app_commands.describe(type='Roll type(wa | ha | ma)', amount='Times to roll')
+    async def first_command(interaction: discord.Interaction, type: str, amount: int):
+        if (interaction.user.id in BLACK_LIST):
+            await interaction.response.send_message('Calla cholo')
+            return
+        if (type not in ['wa', 'ha', 'ma']):
+            await interaction.response.send_message('Roll type is not valid')
+            return
+        await interaction.response.send_message(f'Rolling for {type}!')
+        await responses.handle_response(interaction.channel_id, f'{type} {amount}')
+
+    client.run(BOT_TOKEN)
---
+++ responses.py
@@ -0,0 +1,38 @@
+import requests, time, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL_BASE = 'https://discord.com/api/v9/channels/{}/messages'
+USER_TOKEN = os.getenv('USER_TOKEN')
+
+def handle_response(channel_id: int, user_message: str) -> str:
+    p_user_message = user_message.lower()
+    [command, command_message] = p_user_message.split(' ', 1) if ' ' in user_message else [user_message, None]
+    headers = {
+        'Authorization': USER_TOKEN,
+    }
+    json = {
+        'content': str(command),
+    }
+    match command:
+        case 'cc':
+            return str(command_message)
+        case 'tu' | 'rolls' | 'daily' | 'dk' | 'vote':
+            requests.post(URL_BASE.format(channel_id), json=json, headers=headers, timeout=2.50)
+            return ''
+        case 'wa' | 'ha' | 'ma':
+            try:
+                times_to_roll = int(command_message)
+            except Exception as ex:
+                times_to_roll = 1
+                print(ex)
+            else:
+                if times_to_roll > 10:
+                    times_to_roll = 10
+            for _ in range(times_to_roll):
+                requests.post(URL_BASE.format(channel_id), json=json, headers=headers, timeout=2.50)
+                time.sleep(0.5)
+            return ''
+        case _:
+            return '`Unknown command`'
