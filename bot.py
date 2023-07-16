154bc7f223eabbede533189ff216945fcc1826fa
Leonardo Acevedo <stenoner90@gmail.com>
2023-07-16 17:59:49 -0500

    fixed command recognition when prefix length > 1

--- bot.py
+++ bot.py
@@ -5,16 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-WHITE_LIST = [224957447096238081]
-BLACK_LIST = [283333073532223488]
+USERS_WHITE_LIST = [224957447096238081]
+USERS_BLACK_LIST = [283333073532223488]
 CURRENT_PREFIX = '?'
 
 async def send_message(message: discord.Message, user_mesage: str, is_private: bool):
     try:
         response = responses.handle_response(message.channel.id, user_mesage)
         if len(response) > 0:
-            await message.author.send(response) if is_private else await message.channel.send(response)
-            
+            await message.author.send(response) if is_private else await message.channel.send(response) 
     except Exception as e:
         print(e)
 
@@ -37,7 +36,7 @@ def run_discord_bot():
         
     @client.event
     async def on_message(message: discord.Message):
-        if message.author.id not in WHITE_LIST or message.author == client.user or message.author.bot:
+        if message.author.id not in USERS_WHITE_LIST or message.author == client.user or message.author.bot:
             return
         user_name = str(message.author.name)
         user_message = str(message.content)
@@ -46,25 +45,25 @@ def run_discord_bot():
         print(f'{user_name} said: {user_message} in {channel}')
         
         if len(user_message) > 0 and re.search(f'^\{CURRENT_PREFIX}.+', user_message):
-            command = user_message[1:]
+            command = user_message[len(CURRENT_PREFIX):]
             await send_message(message, command, is_private=False)
             
     @client.event
     async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
-        if user.id not in WHITE_LIST or user.bot:
+        if user.id not in USERS_WHITE_LIST or user.bot:
             return
         await add_reaction(reaction, user)
     
     @tree.command(name='roll', description= 'Mudae rolls', guild=discord.Object(id=322616525527973890))
     @app_commands.describe(type='Roll type(wa | ha | ma)', amount='Times to roll')
     async def first_command(interaction: discord.Interaction, type: str, amount: int):
-        if (interaction.user.id in BLACK_LIST):
+        if (interaction.user.id in USERS_BLACK_LIST):
             await interaction.response.send_message('Calla cholo')
             return
         if (type not in ['wa', 'ha', 'ma']):
             await interaction.response.send_message('Roll type is not valid')
             return
         await interaction.response.send_message(f'Rolling for {type}!')
-        await responses.handle_response(interaction.channel_id, f'{type} {amount}')
+        responses.handle_response(interaction.channel_id, f'{type} {amount}')
 
     client.run(BOT_TOKEN)
